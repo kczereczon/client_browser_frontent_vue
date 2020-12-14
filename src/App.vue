417cdf6c7993564c5aa21554cc3859b3f7f22b19
magda9906 <magdalena.czechura@onet.pl>
2020-12-15 00:13:53 +0100

    poprawiona kolejność

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <vk-navbar>
       <vk-navbar-nav>
         <router-link tag="li" :to="{name: 'Contractors'}"><a href="">KONTRAHENCI</a> </router-link>
-        <router-link tag="li" :to="{name: 'Contacts'}"><a href="">KONTAKTY</a> </router-link>
         <router-link tag="li" :to="{name: 'Departaments'}"><a href="">DEPARTAMENTY</a> </router-link>
+        <router-link tag="li" :to="{name: 'Contacts'}"><a href="">KONTAKTY</a> </router-link>
       </vk-navbar-nav>
     </vk-navbar>
     <router-view />
